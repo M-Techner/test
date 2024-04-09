0017094cee2456c17d8d694af9426aff4f50652d
Mags8934 <maggie.werning@gmail.com>
2024-04-09 16:51:01 -0500

    rebuild

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent, ContactComponent, AboutComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- test/src/app/app.routes.ts
+++ test/src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LandingComponent } from './landing/landing.component';
+import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
+import { OtherComponent } from './other/other.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: "about", component: AboutComponent},
     {path: "contact", component: ContactComponent},
-    {path: "home", component: LandingComponent}
+    {path: "home", component: LandingComponent},
+    {path: "buy", component: BuyComponent},
+    {path: "sell", component: SellComponent},
+    {path: "other", component: OtherComponent},
+    { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
