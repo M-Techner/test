5169fa8a0587efa28b05b9071ab67304233bef2d
Mags8934 <maggie.werning@gmail.com>
2024-04-09 20:53:46 -0500

    need to push?

--- test/src/app/app.routes.ts
+++ test/src/app/app.routes.ts
@@ -14,5 +14,6 @@ export const routes: Routes = [
     {path: "buy", component: BuyComponent},
     {path: "sell", component: SellComponent},
     {path: "other", component: OtherComponent},
+    {path: "index", component: LandingComponent},
     { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
